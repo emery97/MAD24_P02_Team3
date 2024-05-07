5efb4d4b5282d1cc9730d50e528c7a3a3d0f8e94
kohye <kohyechyang11@gmail.com>
2024-05-07 20:35:44 +0800

    Login and create account page done (using fragment)

---
+++ app/src/main/java/sg/edu/np/mad/madassignment/MainActivity.java
@@ -0,0 +1,49 @@
+package sg.edu.np.mad.madassignment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView signup = findViewById(R.id.signup);
+        TextView register = findViewById(R.id.register);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fraglayout, new SignIn());  // Load SignIn
+                transaction.commit();  // Commit
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fraglayout, new Register());  // Load Register
+                transaction.commit();  // Commit
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/madassignment/SignIn.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.madassignment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class SignIn extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);  // Correct layout for this fragment
+    }
+}
