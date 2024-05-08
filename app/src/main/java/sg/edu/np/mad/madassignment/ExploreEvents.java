4e82e10be8f565e86582a2815e82d17d319f4160
cctansh <s10260078@connect.np.edu.sg>
2024-05-08 14:51:03 +0800

    added explore event page

---
+++ app/src/main/java/sg/edu/np/mad/madassignment/ExploreEvents.java
@@ -0,0 +1,48 @@
+package sg.edu.np.mad.madassignment;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ExploreEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_explore_events);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ArrayList<Event> tempEvents = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            String name = "Name" + new Random().nextInt(100);
+            String date = "Date" + new Random().nextInt(100);
+            int img = R.drawable.img;
+            tempEvents.add(new Event(img,name,date));
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.exploreView);
+        EventAdapter mAdapter = new EventAdapter(tempEvents);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+    }
+}
