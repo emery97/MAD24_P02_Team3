07d601b4bd8dc2d0f4d75fa50fe7aadb0a7820a1
kohye <kohyechyang11@gmail.com>
2024-05-13 15:23:36 +0800

    (YC & IVAN) Completed the event details page

---
+++ app/src/main/java/sg/edu/np/mad/TicketFinder/EventDetails.java
@@ -0,0 +1,48 @@
+package sg.edu.np.mad.TicketFinder;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_event_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button showMapButton = findViewById(R.id.showMapButton);
+        showMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create and display a dialog
+                Dialog mapDialog = new Dialog(EventDetails.this);
+                mapDialog.setContentView(R.layout.map_dialog);
+                ImageView mapImage = mapDialog.findViewById(R.id.mapImageView);
+                mapImage.setImageResource(R.drawable.seat_map);
+                Button closeButton = mapDialog.findViewById(R.id.closeButton);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mapDialog.dismiss();
+                    }
+                });
+                mapDialog.show();
+            }
+        });
+    }
+}
