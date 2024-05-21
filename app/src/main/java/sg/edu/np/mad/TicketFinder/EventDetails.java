69dab4dbee541ac6ebe9431ac9a631c026beae01
cctansh <s10260078@connect.np.edu.sg>
2024-05-21 19:13:13 +0800

    eventDetails displays event data

--- app/src/main/java/sg/edu/np/mad/TicketFinder/EventDetails.java
+++ app/src/main/java/sg/edu/np/mad/TicketFinder/EventDetails.java
@@ -7,12 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class EventDetails extends AppCompatActivity {
 
@@ -27,10 +30,51 @@ public class EventDetails extends AppCompatActivity {
             return insets;
         });
 
+        // get event object
         Intent intent = getIntent();
         Event eventObj = (Event) getIntent().getSerializableExtra("event");
         Log.i("event", eventObj.toString()); // testing
 
+        // initialise all views
+        TextView title = findViewById(R.id.eventTitle);
+        TextView caption = findViewById(R.id.eventCaption);
+        TextView artist = findViewById(R.id.eventArtist);
+        TextView genre = findViewById(R.id.eventGenre);
+        TextView timing = findViewById(R.id.eventTiming);
+        TextView venue = findViewById(R.id.eventVenue);
+        TextView description = findViewById(R.id.eventDescription);
+        TextView price = findViewById(R.id.eventTicketPrice);
+        TextView salesTiming = findViewById(R.id.eventGeneralSales);
+
+        // test
+        String inputDate = eventObj.getDate(); // example input
+
+        // Define the date formatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // Parse the input date string to LocalDate
+        LocalDate date = LocalDate.parse(inputDate, formatter);
+
+        // Subtract one month from the date
+        LocalDate newDate = date.minusMonths(1);
+
+        // Format the new date back to a string
+        String outputDate = newDate.format(formatter);
+
+
+        // setting text
+        title.setText(eventObj.getTitle());
+        caption.setText(eventObj.getCaption());
+        artist.setText(eventObj.getArtist());
+        genre.setText(eventObj.getGenre());
+        timing.setText(eventObj.getDate() + ", " + eventObj.getTime());
+        venue.setText(eventObj.getVenue());
+        description.setText(eventObj.getDescription());
+        price.setText("$" + String.format("%.2f",eventObj.getPrice())); // set price to string with 2dp
+        salesTiming.setText(outputDate);
+
+
+        // show map button
         Button showMapButton = findViewById(R.id.showMapButton);
         showMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
