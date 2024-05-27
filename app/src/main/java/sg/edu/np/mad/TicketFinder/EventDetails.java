edbf0a8825bfd447c598cedcb48e56c0e26ee7cb
emery97 <S10258645@connect.np.edu.sg>
2024-05-27 13:48:32 +0800

    WY : dropdown for buy tickets not done

--- app/src/main/java/sg/edu/np/mad/TicketFinder/EventDetails.java
+++ app/src/main/java/sg/edu/np/mad/TicketFinder/EventDetails.java
@@ -14,9 +14,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.bumptech.glide.Glide;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -39,7 +36,6 @@ public class EventDetails extends AppCompatActivity {
         Log.i("event", eventObj.toString()); // testing
 
         // initialise all views
-        ImageView eventImg = findViewById(R.id.eventImg);
         TextView title = findViewById(R.id.eventTitle);
         TextView caption = findViewById(R.id.eventCaption);
         TextView artist = findViewById(R.id.eventArtist);
@@ -61,10 +57,6 @@ public class EventDetails extends AppCompatActivity {
         // format sales period date to string
         String salesDateString = salesDate.format(formatter);
 
-        // set image
-        Glide.with(this)
-                .load(eventObj.getImgUrl())
-                        .into(eventImg);
 
         // setting text
         title.setText(eventObj.getTitle());
@@ -87,7 +79,7 @@ public class EventDetails extends AppCompatActivity {
                 Dialog mapDialog = new Dialog(EventDetails.this);
                 mapDialog.setContentView(R.layout.map_dialog);
                 ImageView mapImage = mapDialog.findViewById(R.id.mapImageView);
-                mapImage.setImageResource(R.drawable.seat_map);
+                mapImage.setImageResource(R.drawable.seat_map_with_color_legend);
                 Button closeButton = mapDialog.findViewById(R.id.closeButton);
                 closeButton.setOnClickListener(new View.OnClickListener() {
                     @Override
