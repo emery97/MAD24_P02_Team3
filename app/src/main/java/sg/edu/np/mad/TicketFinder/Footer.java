2acb1750634a9603b3b7fcf3d117da51f413e786
NgJoeYi <s10262850@connect.np.edu.sg>
2024-06-01 14:47:00 +0800

    comments to footer java

--- app/src/main/java/sg/edu/np/mad/TicketFinder/Footer.java
+++ app/src/main/java/sg/edu/np/mad/TicketFinder/Footer.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 public class Footer {
     public static void setUpFooter(final Activity activity) {
+        // Finding the footer icons by their IDs
         ImageButton homeIcon = activity.findViewById(R.id.footerHomeIcon);
         ImageButton searchIcon = activity.findViewById(R.id.footerSearchIcon);
         ImageButton bookingIcon = activity.findViewById(R.id.footerBookingIcon);
@@ -14,32 +15,37 @@ public class Footer {
 
         homeIcon.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                // Create an intent to start the homepage activity
                 Intent intent = new Intent(activity, homepage.class);
-                activity.startActivity(intent);
+                activity.startActivity(intent); // Start the homepage activity
             }
         });
 
         searchIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Set click listener for the search icon
                 Intent intent = new Intent(activity, ExploreEvents.class);
-                activity.startActivity(intent);
+                activity.startActivity(intent); // Start the ExploreEvents activity
             }
         });
 
         // ---- replace class when purchase history / booking history initialised
+        // **** REPLACED ****
         bookingIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create an intent to start the BookingHistoryDetails activity
                 Intent intent = new Intent(activity, BookingHistoryDetails.class);
-                activity.startActivity(intent);
+                activity.startActivity(intent); // Start the BookingHistoryDetails activity
             }
         });
         accountIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create an intent to start the profilePage activity
                 Intent intent = new Intent(activity, profilePage.class);
-                activity.startActivity(intent);
+                activity.startActivity(intent); // Start the profilePage activity
             }
         });
     }
