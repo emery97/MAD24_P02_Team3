5e6bb580295674e4ee22a69d29f9529aee8de979
emery97 <S10258645@connect.np.edu.sg>
2024-07-07 01:50:55 +0800

    WY : trouble connecting to google calendar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    id("com.android.application")
     id("com.google.gms.google-services")
 }
 
@@ -30,29 +30,31 @@ android {
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation(libs.glide)
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.appcheck.playintegrity)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.activity:activity:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-auth:21.0.1")
+    implementation("com.google.firebase:firebase-firestore:24.3.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     implementation("com.paypal.sdk:paypal-android-sdk:2.16.0")
     implementation("com.stripe:stripe-java:25.0.0")
     implementation("com.stripe:stripe-android:20.44.0")
     implementation("com.android.volley:volley:1.2.1")
-    implementation("com.google.firebase:firebase-firestore:25.0.0")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-storage:21.0.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-appcheck-playintegrity")
     implementation("com.github.chrisbanes:PhotoView:2.3.0")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.code.gson:gson:2.8.8")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
+    // For Google Auth and Calendar integrations
+    implementation("com.google.android.gms:play-services-auth:20.3.0")
+
+    // For easy permissions
+    implementation("pub.devrel:easypermissions:3.0.0")
 }
